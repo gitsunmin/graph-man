43b483829d6149caf008566f22d09462126c7d2f
김선민 <tjsals116@gmail.com>
2024-06-16 23:58:46 +0900

    ✨ Add sendGraphQL function for sending GraphQL queries

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,47 @@
 import * as vscode from 'vscode';
 import { EnvironmentTreeProvider } from './views/environmentTree';
 import { GraphqlsTreeProvider } from './views/graphqlsTree';
+import { getConfig } from './utils/loadConfig';
+
+let outputChannel: vscode.OutputChannel;
+
+function sendGraphQL({
+    query = '',
+    variables = {},
+    endpoint,
+    headers,
+}: {
+    query: string;
+    variables: Record<string, any>;
+    endpoint: string;
+    headers: Record<string, string>;
+}) {
+
+    fetch(endpoint, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', ...headers },
+        body: JSON.stringify({
+            query,
+            variables,
+        }),
+    })
+        .then(response => response.json())
+        .then(data => {
+
+            // Terminal 생성
+            outputChannel.show(true);
+            outputChannel.clear();
+            outputChannel.appendLine('Hello World');
+            outputChannel.appendLine(JSON.stringify(data, null, 2));
+        })
+        .catch(error => {
+            vscode.window.showErrorMessage(error.message);
+        });
+}
 
 export function activate(context: vscode.ExtensionContext) {
+    outputChannel = vscode.window.createOutputChannel('Graph Man');
+
     const environmentTreeProvider = new EnvironmentTreeProvider(context, vscode.workspace.rootPath || '');
 
     const environmentTreeview = vscode.window.createTreeView('graph-man-environment', { treeDataProvider: environmentTreeProvider });
@@ -13,9 +52,20 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(
         vscode.commands.registerCommand('graph-man.refresh-environment', () => environmentTreeProvider.refresh()),
         vscode.commands.registerCommand('graph-man.refresh-header', () => graphqlsTreeProvider.refresh()),
-        environmentTreeview
-    );
+        vscode.commands.registerCommand('graph-man.send-graphql', async () => {
+            const selectedEnvironment = context.globalState.get('selectedEnvironment') as string ?? '';
+            const query = vscode.window.activeTextEditor?.document.getText() ?? '';
+            const config = getConfig(vscode.workspace.rootPath || '');
+            const endpoint = config.environment[selectedEnvironment]?.url ?? '';
 
+            sendGraphQL({
+                query,
+                variables: {},
+                endpoint,
+                headers: {},
+            });
+        }),
+    );
 
     environmentTreeview.onDidChangeSelection(e => {
         if (e.selection.length > 0) {
@@ -23,14 +73,6 @@ export function activate(context: vscode.ExtensionContext) {
             environmentTreeProvider.selectEnvironment(selectedTreeItem.label);
         }
     });
-}
-
-async function selectEnvironment(node: any) {
-    const selected = await vscode.window.showQuickPick([node.label], { placeHolder: 'Select Environment' });
-    if (selected) {
-        vscode.window.showInformationMessage(`Selected Environment: ${selected}`);
-        vscode.commands.executeCommand('setContext', 'selectedEnvironment', selected);
-    }
 }
 
 export function deactivate() { }
